368e68fed6dca3599c4a1021e8d39fdd828cc8f5
Asus <lilla.papp@stud.uni-corvinus.hu>
2022-10-06 14:59:15 +0200

    4/6feladat

--- 4het/4het/Form1.cs
+++ 4het/4het/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Reflection;
+using System.Data.Entity.Migrations.Model;
 
 namespace _4het
 {
@@ -16,17 +17,48 @@ namespace _4het
     {
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
+        Excel.Application xlApp;
+        Excel.Workbook xlWb;
+        Excel.Worksheet xlSheet;
         
         public Form1()
         {
             InitializeComponent();
             LoadData();
+
             
         }
 
         private void LoadData() 
         {
             Flats = context.Flats.ToList();
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+
+                xlWb = xlApp.Workbooks.Add(Missing.Value);
+
+                xlSheet = xlWb.ActiveSheet;
+
+                //CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                xlWb.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWb = null;
+                xlApp = null;
+            }
         }
     }
 }
