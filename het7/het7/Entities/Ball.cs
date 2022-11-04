227cf44317ca0309cbf84d0ab11746ecd29b19ab
b9pn39 <lilla.papp@stud.uni-corvinus.hu>
2022-11-04 10:34:58 +0100

    jatekabsz

--- het7/het7/Entities/Ball.cs
+++ het7/het7/Entities/Ball.cs
@@ -10,27 +10,12 @@ namespace het7.Entities
 {
     public class Ball : Label
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Height = 50;
-            Width = Height;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
 
         private void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
         }
 
-        public void MoveBall()
-        {
-            Left += 1;
-        }
+        
     }
 }
