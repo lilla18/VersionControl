789c5e462690c5ad461b72758f59fb4075f96687
Lilla Papp <lilla.papp@stud.uni-corvinus.hu>
2022-11-10 18:54:25 +0100

    4feladat

--- het8/8het/8het/Form1.cs
+++ het8/8het/8het/Form1.cs
@@ -12,9 +12,13 @@ namespace _8het
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
