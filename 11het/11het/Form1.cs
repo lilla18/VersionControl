2e0c01180b3217ca033abd4e3087d2a188ad1b56
b9pn39 <lilla.papp@stud.uni-corvinus.hu>
2022-12-02 11:17:38 +0100

    negyedik

--- 11het/11het/Form1.cs
+++ 11het/11het/Form1.cs
@@ -104,5 +104,36 @@ namespace _11het
 
             return deaths;
         }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Kor == age
+                             select x.DeathP).FirstOrDefault();
+
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Kor == age
+                                 select x.Probability).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
     }
 }
