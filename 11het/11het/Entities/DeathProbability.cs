b473ff7dc0479b809ccc9a910573def1c65d375a
b9pn39 <lilla.papp@stud.uni-corvinus.hu>
2022-12-02 10:13:47 +0100

    elso

---
+++ 11het/11het/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11het.Entities
+{
+    public class DeathProbability
+    {
+        public Gender Gender { get; set; }
+        public int Kor { get; set; }
+        public double DeathP { get; set; }
+    }
+}
